c96328401ffbcc3e057a3a4241ada4eb1fffd2ed
Momo <momoguirassy224@gmail.com>
2023-09-19 15:59:38 +0200

    creation de notre premier api

---
+++ API/API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("Bonjour/{nom}")]
+        public IActionResult Get(string nom)
+        {
+            if(String.IsNullOrEmpty(nom))
+            {
+                return BadRequest();
+            }
+            return Ok($"Bonjour {nom} !");
+        }
+    }
+}
