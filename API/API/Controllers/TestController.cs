a1d85717a78ba478b5effb6651ce18261d03fdc7
Momo <momoguirassy224@gmail.com>
2023-09-19 16:48:52 +0200

    documentation d'api

--- API/API/Controllers/TestController.cs
+++ API/API/Controllers/TestController.cs
@@ -3,10 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+
+    /// <summary>
+    /// Controlleur de test
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
+        /// <summary>
+        ///  Action pour dire bonjour 
+        /// </summary>
+        /// <param name="nom"> Nom à qui dire bonjour </param>
+        /// <returns> Retourne Bonjour suivi du nom de la peersonne </returns>
         [HttpGet("Bonjour/{nom}")]
         public IActionResult Get(string nom)
         {
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace API
 {
     public class Program
@@ -12,7 +14,21 @@ namespace API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c=>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Démonstration Web API",
+                    Description = "API de demonstration de la création d'une web API.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mohamed Guirasssy",
+                        Email = "contact@mail.fr"
+                    }
+                });
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, "Swagger.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
 
             var app = builder.Build();
 
@@ -20,7 +36,11 @@ namespace API
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Démo API");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
